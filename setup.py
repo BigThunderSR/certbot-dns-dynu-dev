cf8cae607460ceb3d8641ceba189dfead4624c2f
BigThunderSR <17056173+BigThunderSR@users.noreply.github.com>
2024-12-03 20:54:08 -0600

    v0.0.6

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from os import path
 from setuptools import setup
 from setuptools import find_packages
 
-VERSION = "0.0.5"
+VERSION = "0.0.6"
 
 with open('README.md', 'r', encoding='utf-8') as f:
     long_description = f.read()
